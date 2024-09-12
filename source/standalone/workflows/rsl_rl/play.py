5c240d45948b3b610a85b5a453eff57ba00ec756
Jean Tampon <jean.tampon@gmail.com>
2024-09-12 12:12:10 +0200

    Working

--- source/standalone/workflows/rsl_rl/play.py
+++ source/standalone/workflows/rsl_rl/play.py
@@ -97,13 +97,13 @@ def main():
     policy = ppo_runner.get_inference_policy(device=env.unwrapped.device)
 
     # export policy to onnx/jit
-    export_model_dir = os.path.join(os.path.dirname(resume_path), "exported")
+    """export_model_dir = os.path.join(os.path.dirname(resume_path), "exported")
     export_policy_as_jit(
         ppo_runner.alg.actor_critic, ppo_runner.obs_normalizer, path=export_model_dir, filename="policy.pt"
     )
     export_policy_as_onnx(
         ppo_runner.alg.actor_critic, normalizer=ppo_runner.obs_normalizer, path=export_model_dir, filename="policy.onnx"
-    )
+    )"""
 
     # reset environment
     obs, _ = env.get_observations()
